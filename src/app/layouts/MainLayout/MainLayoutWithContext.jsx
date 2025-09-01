955fb8580f25a8dc2d341e7a420828b3de2c87e6
AbelThomas-abacies <abel.thomas@abacies.in>
2025-09-01 14:48:33 +0530

    changes

--- src/app/layouts/MainLayout/MainLayoutWithContext.jsx
+++ src/app/layouts/MainLayout/MainLayoutWithContext.jsx
@@ -19,7 +19,7 @@ export default function MainLayoutWithContext() {
   const { pathname } = useLocation();
   const [isNewTicketOpen, setIsNewTicketOpen] = useState(false);
 
-  const isTicketChat = /^\/support-user\/[^/]+$/.test(pathname);
+  // const isTicketChat = /^\/support-user\/[^/]+$/.test(pathname);
   
   // Get context values for state management
   const documentsContext = useContext(DocumentsContext);
@@ -62,10 +62,10 @@ export default function MainLayoutWithContext() {
         isExpanded ? "lg:ml-0" : "lg:ml-0"
       )}>
         <DynamicHeader {...getHeaderProps()} />
-        <div  className={`flex-1 overflow-y-auto ${
-          isTicketChat ? "" : "p-4 sm:p-6"
-        }`}>
-        {/* <div className="flex-1 overflow-y-auto p-4 sm:p-6"> */}
+
+        {/* here i have removed to avoid uncessary scroll */}
+        <div className="flex-1  overflow-y-auto p-4 sm:p-6">
+
           <Outlet />
         </div>
         <NewTicketModal
