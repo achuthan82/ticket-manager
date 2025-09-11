0d9879e4f4f8df434419e6305eca72e50483a008
AchuthanVadassery <achuthan.v@cubettech.com>
2025-09-11 18:04:06 +0530

    changes

--- src/configs/auth.config.js
+++ src/configs/auth.config.js
@@ -2,9 +2,10 @@
  * DocuPrompt Hub API Configuration
 **/
 // Staging
-export const JWT_HOST_TEST_API = "https://shieldnest-backend-staging-437a38552d5f.herokuapp.com";
-export const JWT_HOST_API='https://shield-tickets-backend-4bfb1f52b122.herokuapp.com/'
-export const REDIRECT_URL = 'http://shieldnest-new.s3-website-us-east-1.amazonaws.com/'
+// export const JWT_HOST_TEST_API = "https://shieldnest-backend-staging-437a38552d5f.herokuapp.com";
+// export const JWT_HOST_API='https://shield-tickets-backend-4bfb1f52b122.herokuapp.com/'
+// export const REDIRECT_URL = 'http://shieldnest-new.s3-website-us-east-1.amazonaws.com/'
 // Live
-// export const JWT_HOST_API='https://shield-tickets-backend-live-887a07599ccb.herokuapp.com/'
-// export const JWT_HOST_TEST_API = "https://shieldnest-backend-production-cfedfcca08b0.herokuapp.com/";
+export const JWT_HOST_API='https://shield-tickets-backend-live-887a07599ccb.herokuapp.com/'
+export const JWT_HOST_TEST_API = "https://shieldnest-backend-production-cfedfcca08b0.herokuapp.com/";
+export const REDIRECT_URL = 'https://hub.shieldnestleads.com/'
