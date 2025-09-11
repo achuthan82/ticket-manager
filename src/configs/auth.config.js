62735cb66b82553d630910797881a992c6929848
AchuthanVadassery <achuthan.v@cubettech.com>
2025-09-11 14:57:16 +0530

    changes

--- src/configs/auth.config.js
+++ src/configs/auth.config.js
@@ -4,6 +4,7 @@
 // Staging
 export const JWT_HOST_TEST_API = "https://shieldnest-backend-staging-437a38552d5f.herokuapp.com";
 export const JWT_HOST_API='https://shield-tickets-backend-4bfb1f52b122.herokuapp.com/'
+export const REDIRECT_URL = 'http://shieldnest-new.s3-website-us-east-1.amazonaws.com/'
 // Live
 // export const JWT_HOST_API='https://shield-tickets-backend-live-887a07599ccb.herokuapp.com/'
 // export const JWT_HOST_TEST_API = "https://shieldnest-backend-production-cfedfcca08b0.herokuapp.com/";
